b27db753db6a93ce025fea4f8a6fab0a54f9bdd6
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-10-24 15:34:10 -0400

    added schema and seed files

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,26 @@
+DROP DATABASE IF EXISTS employee;
+CREATE DATABASE employee;
+USE employee;
+
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY,
+    id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-    id INTEGER PRIMARY KEY,
+CREATE TABLE roles (
+    id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INTEGER UNSIGNED NOT NULL
+    salary DECIMAL (6,0) UNSIGNED NOT NULL,
+    department_id INTEGER UNSIGNED NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INTEGER PRIMARY KEY,
+    id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER UNSIGNED NOT NULL,
-    manager_id INTEGER UNSIGNED NOT NULL
-    
+    manager_id INTEGER UNSIGNED,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
